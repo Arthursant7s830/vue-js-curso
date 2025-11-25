6dad4c00e5ae54fcbc844592ab033bc5c90c228c
Arthur Santos Lima <135383243+Arthursant7s830@users.noreply.github.com>
2025-11-25 13:59:40 -0300

    Adicionado input

--- Project 3 - Todo List - Initial/scripts.js
+++ Project 3 - Todo List - Initial/scripts.js
@@ -13,7 +13,23 @@ var todos = [
 const todoApp = {
     data() {
         return {
-            todo: window.todos
+            todo: window.todos,
+            newTodo: {
+                done: false
+            } 
+        }
+    },
+    methods: {
+        addTodo: function () {
+            if (this.newTodo.text) {
+
+                this.todo.push(this.newTodo)
+                this.newTodo = {
+                    done: false
+                }
+            } else {
+                alert("To-do text is required!")
+            }
         }
     }
 }
