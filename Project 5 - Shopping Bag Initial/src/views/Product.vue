3799e5c80a6c13c5f0581a52664dd0e18ca8f539
Arthur Santos Lima <135383243+Arthursant7s830@users.noreply.github.com>
2025-12-15 17:33:53 -0300

    criado projeto inicial

---
+++ Project 5 - Shopping Bag Initial/src/views/Product.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        this is th product page
+    </div>
+  </template>
+  
+  <script>
+  
+  export default {
+    name: 'Product',
+  
+    methods: {
+     
+    },
+   
+  }
+  </script>
+  
+  <style lang="scss">
+  
+  
+  
+  </style>
+  
